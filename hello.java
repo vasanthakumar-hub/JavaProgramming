258638f8cbadd7c489df6858aabc8da06949ab0a
vasanthakumar-hub <vasanthakumar0969@gmail.com>
2025-03-07 08:39:06 +0530

    java intro notes added

--- hello.java
+++ hello.java
@@ -4,3 +4,8 @@ class hello{
         System.out.print("Hello World");
     }
 }
+
+// Notes:
+//     1.We Write a Code In English.
+//     2.Code is Sent To Java Compiler to Convert into Byte Code Which is Readable by a Computer Machine Language.(javac hello.java)
+//     3.Then It is Sent To JVM (Java Virutual Machine) Which Makes the Code to be Machine Readable Code and to Execute. (java hello.java)
