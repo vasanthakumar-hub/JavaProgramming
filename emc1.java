9cc7edfd9b9bd878d978a0969c44516aec079241
vasanthakumar-hub <vasanthakumar0969@gmail.com>
2025-03-11 12:57:39 +0530

    variables & datatypes

---
+++ emc1.java
@@ -0,0 +1,15 @@
+public class emc1 {
+    public static void main(String args[])
+    {
+        int a = 5 ;
+        int b = 9 ;
+        int c = 15 ;
+        int d = a+b+c ;
+        System.out.print(d);
+        
+    }
+}
+
+// Note:
+//     1.Values Entered Inside "Double Quotation" Will not be Considered as a Variable.
+//     2.Variable name(a,b,c,d), datatypes?(int,String,...),Operators(+,-,*,/)
